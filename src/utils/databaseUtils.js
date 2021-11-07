35ceec1f471a94b6f44dd106364bcb3e902764b4
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-11-07 20:44:16 +0100

    add some production database dialectOptions

--- src/utils/databaseUtils.js
+++ src/utils/databaseUtils.js
@@ -3,7 +3,14 @@ const { Sequelize } = require("sequelize");
 let sequelize;
 
 if (process.env.NODE_ENV === "production") {
-    sequelize = new Sequelize(process.env.DATABASE_URL);
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    });
 } else {
     sequelize = new Sequelize("test", "user", "pass", {
         dialect: "sqlite",
