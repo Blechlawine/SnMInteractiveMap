ebb40ac54f62f3e26a468c3519382b7b28c2fbc8
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-11-07 21:31:43 +0100

    more advanced seed command

--- src/seed.js
+++ src/seed.js
@@ -3,7 +3,7 @@ const sequelize = require("./utils/databaseUtils");
 const fs = require("fs");
 const { Type, Category, Pin } = require("./model");
 
-const seedFilePath = "./data/seed.json";
+const seedFilePath = process.argv[2] || "./data/seed.json";
 
 sequelize.drop().then(() => {
     sequelize
@@ -11,16 +11,35 @@ sequelize.drop().then(() => {
         .then(async () => {
             console.log("Connected to database successfully!");
             let seed = JSON.parse(fs.readFileSync(seedFilePath));
-            seed.pins.forEach((pin) => {
-                Pin.create(pin, {
-                    include: [
-                        {
-                            model: Type,
-                            include: [Category],
-                        },
-                    ],
+            if (seed.pins) {
+                seed.pins.forEach((pin) => {
+                    Pin.create(pin, {
+                        include: [
+                            {
+                                model: Type,
+                                include: [Category],
+                            },
+                        ],
+                    });
                 });
-            });
+            }
+            if (seed.categories) {
+                seed.categories.forEach(cat => {
+                    Category.create(cat, {
+                        include: [Type],
+                    });
+                });
+            }
+            if (seed.types) {
+                seed.types.forEach((type) => {
+                    Type.create(type, {
+                        include: [
+                            Pin,
+                            Category,
+                        ],
+                    });
+                });
+            }
         })
         .catch((err) => {
             console.log("Unable to connect to database: ", err);
