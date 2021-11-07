da484deb41260aa0c0d740cbfa9c097b57405b2e
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-11-07 21:10:00 +0100

    add production cors origin

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,4 +1,7 @@
 module.exports = (req, res, next) => {
-    res.set("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.set(
+        "Access-Control-Allow-Origin",
+        process.env.NODE_ENV === "production" ? "https://snminteractivemap.herokuapp.com/" : "http://localhost:8080"
+    );
     return next();
 };
