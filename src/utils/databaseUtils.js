950b17279b7b020705cd6544a95e639eaddd2812
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-11-07 20:14:47 +0100

    make production ready

--- src/utils/databaseUtils.js
+++ src/utils/databaseUtils.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize("test", "user", "pass", {
-    dialect: "sqlite",
-    storage: "./data/database.sqlite",
+    dialect: process.env.NODE_ENV === "production" ? "postgres" : "sqlite",
+    storage: process.env.NODE_ENV === "production" ? process.env.DATABASE_URL : "./data/database.sqlite",
 });
 
 module.exports = sequelize;
