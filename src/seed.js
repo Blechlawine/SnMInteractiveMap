dd4ba3fc8a7c59d63632be7c14da3158ff2a6d1a
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-11-08 20:30:06 +0100

    dont drop tables before seeding

--- src/seed.js
+++ src/seed.js
@@ -5,43 +5,38 @@ const { Type, Category, Pin } = require("./model");
 
 const seedFilePath = process.argv[2] || "./data/seed.json";
 
-sequelize.drop().then(() => {
-    sequelize
-        .sync()
-        .then(async () => {
-            console.log("Connected to database successfully!");
-            let seed = JSON.parse(fs.readFileSync(seedFilePath));
-            if (seed.pins) {
-                seed.pins.forEach((pin) => {
-                    Pin.create(pin, {
-                        include: [
-                            {
-                                model: Type,
-                                include: [Category],
-                            },
-                        ],
-                    });
+sequelize
+    .sync()
+    .then(async () => {
+        console.log("Connected to database successfully!");
+        let seed = JSON.parse(fs.readFileSync(seedFilePath));
+        if (seed.pins) {
+            seed.pins.forEach((pin) => {
+                Pin.create(pin, {
+                    include: [
+                        {
+                            model: Type,
+                            include: [Category],
+                        },
+                    ],
                 });
-            }
-            if (seed.categories) {
-                seed.categories.forEach(cat => {
-                    Category.create(cat, {
-                        include: [Type],
-                    });
+            });
+        }
+        if (seed.categories) {
+            seed.categories.forEach((cat) => {
+                Category.create(cat, {
+                    include: [Type],
                 });
-            }
-            if (seed.types) {
-                seed.types.forEach((type) => {
-                    Type.create(type, {
-                        include: [
-                            Pin,
-                            Category,
-                        ],
-                    });
+            });
+        }
+        if (seed.types) {
+            seed.types.forEach((type) => {
+                Type.create(type, {
+                    include: [Pin, Category],
                 });
-            }
-        })
-        .catch((err) => {
-            console.log("Unable to connect to database: ", err);
-        });
-});
+            });
+        }
+    })
+    .catch((err) => {
+        console.log("Unable to connect to database: ", err);
+    });
