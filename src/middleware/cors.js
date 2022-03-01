2ef380779f06120ae057c74bdcffbb2e50b654ca
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2022-03-01 20:40:31 +0100

    allow credentials and authorization header

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -3,6 +3,7 @@ module.exports = (req, res, next) => {
         "Access-Control-Allow-Origin",
         process.env.NODE_ENV === "production" ? "https://snminteractivemap.herokuapp.com/" : "http://localhost:8080"
     );
-    res.set("Access-Control-Allow-Headers", "content-type")
+    res.set("Access-Control-Allow-Credentials", true);
+    res.set("Access-Control-Allow-Headers", "content-type, authorization");
     return next();
 };
