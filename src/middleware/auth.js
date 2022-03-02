4cd48ae58fecfec82083443ee52bd44b5e1fd3f1
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2022-03-02 14:53:10 +0100

    check for cookie before rejecting and change http status when cookie exists

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,8 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../model");
+const { parseCookie } = require("../utils").cookie;
+
+const cookieName = "snmInteractiveMapToken";
 
 async function isAuthenticated(req, res, next) {
     const auth = (req.headers && req.headers.authorization) || "";
@@ -25,6 +28,16 @@ async function isAuthenticated(req, res, next) {
 
 function rejectUnauthenticated(req, res, next) {
     if (!res.locals.authenticated || res.locals.user == undefined) {
+        const cookies = parseCookie(req);
+        if (cookies && cookies[cookieName]) {
+            const decoded = jwt.verify(cookies[cookieName], process.env.JWT_REFRESH_SECRET);
+            if (decoded) {
+                res.status(403).json({
+                    message: "Refresh your tokens, then try again",
+                });
+                return;
+            }
+        }
         res.status(401).json({
             message: "Authenticate first",
         });
